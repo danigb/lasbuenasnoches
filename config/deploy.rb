751c13281732044fe3a9ba6f043b13c94a93b9ae
dani <dani@mandarine.(none)>
2009-05-27 18:08:25 +0200

    better deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ after "deploy:update_code", "config:copy_shared_configurations"
 namespace :config do
   desc "copy shared configurations to current"
   task :copy_shared_configurations, :roles => [:app] do
-    %w[database.yml].each do |f|
+    %w[database.yml smtp_gmail.yml].each do |f|
       run "ln -nsf #{shared_path}/config/#{f} #{release_path}/config/#{f}"
     end
   end
