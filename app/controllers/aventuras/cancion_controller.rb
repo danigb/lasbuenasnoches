ed9b50e8ba42c8f4383f26127c5fbb2d55078a4a
dani <danigb@gmail.com>
2009-10-26 19:15:40 +0100

    cortinas y letras

--- app/controllers/aventuras/cancion_controller.rb
+++ app/controllers/aventuras/cancion_controller.rb
@@ -6,14 +6,4 @@ class Aventuras::CancionController < ApplicationController
     render :action => 'entrada', :layout => false
   end
 
-  def la_plaga
-    @video = 'la plaga b1.flv'
-    render :action => 'video', :layout => false
-  end
-
-  def el_fin_del_mundo
-    @video = 'el fin del mundo b1.flv'
-    render :action => 'video', :layout => false
-  end
-
 end
