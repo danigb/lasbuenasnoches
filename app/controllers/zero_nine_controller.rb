faaccace0d6b4718a1f0e85b4688188949063a7c
dani <danigb@gmail.com>
2009-10-26 18:37:16 +0100

    videos

--- app/controllers/zero_nine_controller.rb
+++ app/controllers/zero_nine_controller.rb
@@ -2,11 +2,24 @@
 
 class ZeroNineController < ApplicationController
   layout 'empty'
+
+  VIDEOS = {
+    :names => ['el-fin-del-mundo', 'la-plaga', 'la-jungla', 'tucuman', 'tucuman-exterior',
+      'de-muerte', 'media-vida', 'el-dia-de-tu-boda'],
+    :files => ['el fin del mundo b1.flv', 'la plaga b1.flv','3','4','5','6','7','8']
+  }
+
   def entrada
     
   end
 
-  def entrada2
-    
+  def video
+    path = params[:path].join;
+    index = VIDEOS[:names].index(path)
+    if index
+      @video = VIDEOS[:files][index]
+    else
+      redirect_to videos_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :contents, :controller => 'admin/contents'
 
-  map.root :controller => 'aventuras/cancion', :action => 'entrada'
-  map.connect 'aventuras', :controller => 'aventuras/cancion', :action => 'canciones'
+  map.root :controller => 'zero_nine', :action => 'entrada'
+  map.connect '/video/*path', :controller => 'zero_nine', :action => 'video'
+  map.aventuras '/aventuras', :controller => 'aventuras/cancion', :action => 'canciones'
+  map.videos '/videos', :controller => 'zero_nine', :action => 'videos'
   map.connect 'aventuras/cancion/:action', :controller => 'aventuras/cancion'
   map.connect 'admin/:action', :controller => 'admin/contents'
   map.connect ':action', :controller => 'aventuras/cancion'
