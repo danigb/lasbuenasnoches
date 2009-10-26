f09b4355e2059458a707cc8884157fc82f04c405
dani <danigb@gmail.com>
2009-10-26 19:43:21 +0100

    comments 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :contents, :controller => 'admin/contents'
-
   map.root :controller => 'zero_nine', :action => 'entrada'
   map.connect '/video/*path', :controller => 'zero_nine', :action => 'video'
   map.aventuras '/aventuras', :controller => 'aventuras/cancion', :action => 'canciones'
   map.videos '/videos', :controller => 'zero_nine', :action => 'videos'
+  map.comments '/comentarios', :controller => 'aventuras/cancion', :action => 'add_comment'
   map.connect 'aventuras/cancion/:action', :controller => 'aventuras/cancion'
-  map.connect 'admin/:action', :controller => 'admin/contents'
+  #map.connect 'admin/:action', :controller => 'admin/contents'
   map.connect ':action', :controller => 'aventuras/cancion'
-  map.connect 'nueva/:action', :controller => 'zero_nine'
   
 #  map.connect ':action', :controller => 'web/main'
 end
